e14b44f8ed051bbfaca790608268547701ab11a1
daniiltserin <daniilborowkow@yandex.ru>
2017-09-01 12:05:50 +0600

    Created base template for inex page

---
+++ template-parts/content-home.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package daniil-theme
+ */
+
+?>
+
+<div class="entry-content">
+  <?php
+    /**
+     * all other is deleted. here show from page builder only
+     */
+    the_content();
+  ?>
+</div><!-- .entry-content -->
